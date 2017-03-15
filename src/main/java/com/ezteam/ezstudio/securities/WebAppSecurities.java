a0319a3480119a4ca4a6f5c6c77257e289c58458
ACID-KILLA666 <aurelien.duval6@gmail.com>
2017-03-15 23:22:21 +0100

    Added sample security manager

--- src/main/java/com/ezteam/ezstudio/securities/WebAppSecurities.java
+++ src/main/java/com/ezteam/ezstudio/securities/WebAppSecurities.java
@@ -1,5 +1,12 @@
 package com.ezteam.ezstudio.securities;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 /**
  * @author Aurelien
  * @file WebAppSecurities
@@ -7,5 +14,13 @@ package com.ezteam.ezstudio.securities;
  * @package com.ezteam.ezstudio.securities
  * @date 15-Mar-17
  */
-public class WebAppSecurities {
+@Configuration
+@EnableWebSecurity
+public class WebAppSecurities extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("user").password("password").roles("USER");
+    }
 }
