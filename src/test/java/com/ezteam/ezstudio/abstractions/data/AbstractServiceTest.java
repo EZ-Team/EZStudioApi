552430b12469bec96b322988b6687ebe0f9851c7
Aurelien Duval <aurelien.duval@bnpparibas.com>
2017-04-06 17:54:25 +0200

    Added sample test using PowerMock
    Change-Id: I364ee7e92345bdadb4169c4f2ef9da2050f67d97

--- src/test/java/com/ezteam/ezstudio/abstractions/data/AbstractServiceTest.java
+++ src/test/java/com/ezteam/ezstudio/abstractions/data/AbstractServiceTest.java
@@ -1,8 +1,16 @@
 package com.ezteam.ezstudio.abstractions.data;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import org.mockito.Mockito;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  * @author Aurelien
@@ -11,10 +19,27 @@ import static org.junit.Assert.*;
  * @package com.ezteam.ezstudio.abstractions.data
  * @date 18-Mar-17
  */
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(AbstractAdapter.class)
 public class AbstractServiceTest {
+
+    @InjectMocks AbstractService service;
+
+    @Mock AbstractRepository repository;
+
+    private static final Long DEFAULT_ID = 1l;
+    private static final AbstractEntity DEFAULT_ENTITY = new AbstractEntity();
+    private static final AbstractDTO DEFAULT_DTO = new AbstractDTO();
+
     @Test
     public void shouldFindById() {
+        PowerMockito.spy(AbstractAdapter.class);
+        Mockito.doReturn(DEFAULT_ENTITY).when(repository).findOne(DEFAULT_ID);
+        BDDMockito.given(AbstractAdapter.toDto(DEFAULT_ENTITY)).willReturn(DEFAULT_DTO);
+        AbstractDTO result = service.findById(DEFAULT_ID);
 
+        assertThat(result).isEqualTo(DEFAULT_DTO);
     }
 
     @Test
